9c8fe6462db4a325754c54ddf44f9ecb0543d59e
thomassales <salesthomas96@gmail.com>
2023-11-22 17:25:45 +0100

    Rectification CSS

--- app.js
+++ app.js
@@ -23,34 +23,35 @@ app.use('/static/gif', express.static('static/gif/'))
 app.get('/', function (request, response) {
     const random = get_random(possibilities)
     response.send(` <html>
-                    <head>
-                        <style>
-                        body {
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
-                            height: 100vh;
-                            margin: 0;
-                        }
-                        h1 {
-                            font-size: 6em;
-                            color: white;
-                            text-align: center;
-                            z-index: 1; /* Pour s'assurer que le texte est devant l'image /
-                        }
-                        img {
-                            width: 100%; / L'image prend toute la largeur de la page /
-                            position: absolute;
-                            top: 0;
-                            left: 0;
-                            z-index: 0; / Pour s'assurer que l'image est derrière le texte */
-                        }
-                        </style>
-                </head>
-                <body>
-                    <h1>${random.value}</h1>
-                    <img src='/static/gif/${random.gif_url}' />
-                </body>
+                        <head>
+                            <style>
+                            body {
+                            margin : 0;
+                            }
+                            h1 {
+                                position : absolute;
+                                top: 30%;
+                                left: 40%;
+                                font-size: 6em;
+                                color: white;
+                                text-align: center;
+                                z-index: 1; /* Pour s'assurer que le texte est devant l'image */
+                            }
+                            div {
+
+                                width: 100%; /* L'image prend toute la largeur de la page */
+                                height: 100%;
+                                background-image: url("/static/gif/${random.gif_url}");
+                                background-size : cover;
+                                background-position : center;
+                            }
+                            </style>
+                    </head>
+                    <body>
+                        <h1>${random.value}</h1>
+                        <div class="image"> </div>
+                    
+                    </body>
                 </html>`)
   })
 
